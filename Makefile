619eccea9ef3f9a0d654e241e8dcce4ea3d1dcb6
adrixo <adrianvalrom@gmail.com>
2022-10-09 10:37:24 +0200

    test: add testing

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: all test
+
+test:
+	python3 -m unittest test.test_smart_fridge
---
+++ test/test_smart_fridge.py
@@ -0,0 +1,8 @@
+import unittest
+
+class SmartFridgeShould(unittest.TestCase):
+    def test_unittest(self):
+        assert(True)
+
+if __name__ == '__main__':
+    unittest.main()
